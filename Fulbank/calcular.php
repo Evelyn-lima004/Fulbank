1190b6f1c3f43b353654824cf0151d9512144a91
EVELYN LAVIGNE GOMES DE LIMA <21193113@CFP127ALUNO.LOCAL>
2021-08-13 10:54:50 -0300

    atualizações

--- Fulbank/calcular.php
+++ Fulbank/calcular.php
@@ -1,71 +1,98 @@
-
 <?php
-
-if (isset($_POST["cidadeOrigem"]) && isset($_POST["cidadeDestino"]) && isset($_POST["distancia"]) && isset($_POST["pedagios"])){
-
-    $cidadeOrigem = $_POST["cidadeOrigem"];
-    $cidadeDestino = $_POST["cidadeDestino"];
-    $distancia = $_POST["distancia"];
-    $pedagios = $_POST["pedagios"];
-
-    $valorFrete = 0;
-
-    $valorfrete = $distancia * 6;
-
-    $valorfrete += $pedagios * 9.4;
-
-    // $valorfrete = ($distancia * 6) + ($pedagios * 9.4);
-
-
-
-}else{
-    echo "Você não enviou os dados";
-    exit;
-}
-
-
+ 
+if (isset ($_POST["nomeCompleto"]) && isset($_POST["salarioAtual"])
+    && isset($_POST["genero"]) && isset($_POST["cargo"]))
+{
+    $nome = $_POST["nomeCompleto"];
+    $salarioAtual = $_POST["salarioAtual"];
+    $genero = $_POST["genero"];
+    $cargo = $_POST["cargo"];
+ 
+    $salarioNovo = 0;
+ 
+} else {
+    die ("<main>Você não enviou os dados</main>");
+} 
+ 
+// if ($salarioAtual > 5000) {
+ 
+//     $salarioNovo = $salarioAtual * 1.1;
+// } else {
+//     $salarioNovo = $salarioAtual * 1.2;
+// }
+ 
+$salarioNovo = $salarioAtual > 5000 ? $salarioAtual * 1.1 : $salarioAtual * 1.2;
+ 
 ?>
-
+ 
 <!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Aumento Salarial</title>
+</head>
+<body>
+    <main>
+        <p>
+            <?= $genero === "m" ? "O" : ($genero === "f" ? "A" : "") ?>
+            <?= $nome ?> passará a receber R$
+            <?= number_format($salarioNovo, 2, ",", ".") ?>, 
+            no cargo de <?= $cargo ?>
+        </p>
+    </main>
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculo de frete</title>
+    <title>Aumento Salarial</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <form method="POST" action="calcular.php">
-        <p>Calcular Fretes</p>
+        <p>Aumento Salarial</p>
 
-            <label for="cidadeOrigem">
-                Cidade de Origem
-                <input type="text" id="cidadeOrigem" name="cidadeOrigem" value="<?= $cidadeOrigem ?>" readonly>
-            </label>
-
-            <label for="cidadeDestino">
-                Cidade de Destino
-                <input type="text" id="cidadeDestino" name="cidadeDestino" value="<?= $cidadeDestino ?>" readonly>
-            </label>
-
-        <label for="distancia">
-            Distância em KM
-            <input id="distancia" name="distancia" value="<?= $distancia ?>" readonly>
+        <label for="nome">
+            Nome
+            <input class="texto" type="text" id="nome" name="nome" value="<?= $nome ?>" readonly>
         </label>
 
-        <label for="pedagios">
-            Quantidade de Pedágios
-            <input  id="pedagios" name="pedagios" value="<?= $pedagios ?>" readonly>
+        <label for="salario">
+            Antigo Salário em R$
+            <input class="texto" type="number" id="salario" name="salario" value="<?= $salario ?>" readonly>
+        </label>
+
+        <div class="genero">
+            <label>Gênero:</label>
+            <label for="masculino">
+                <input type="radio"  name="genero" checked value="<?= $genero ?>"><?= $genero ?>
+            </label> 
+
+            <br>
+            <br>
+            <br>
+
+        <label id="cargosLabel" for="cargos">
+            Cargos
+            <select id="cargos" name="cargos" disabled>
+                <option id="selecione" selected value=""><?= $cargos ?></option>
+            </select>
         </label>
 
 
-        <p class="total">A viagem de <?= $cidadeOrigem ?> à <?= $cidadeDestino ?> irá custar o valor total de R$<em><?= number_format($valorfrete,2, ".", ".") ?></em></p>
+        <textarea id="total" cols="22" rows="4" readonly>Parabéns <?= $nome ?>! Você passará a receber R$<?= $novoSalario ?> no cargo de <?= $cargos ?>!</textarea>
 
     </form>
 
 </body>
 
-</html>
+</html> -->
